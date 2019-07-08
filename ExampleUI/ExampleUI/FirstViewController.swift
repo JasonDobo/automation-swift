237ab50493edcbf82d856e6fa5033a9fb5f6ad14
Jason Dobo <jason.dobo@gmail.com>
2019-07-08 14:13:36 +0100

    Example tab based UI using auto generated code from Xcode with unit and UI test classes

---
+++ ExampleUI/ExampleUI/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ExampleUI
+//
+//  Created by Jason Dobo on 08/07/2019.
+//  Copyright © 2019 Jason Dobo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
